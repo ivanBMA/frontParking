49852a01599ed9ce0931d8dded8bdf708de7d18c
Iván Moros Pérez <ivan.moros@dreams.es>
2024-05-14 09:27:32 +0200

    segundo
    se mete codigo js y css

--- src/app/primera-vista/primera-vista.component.ts
+++ src/app/primera-vista/primera-vista.component.ts
@@ -9,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class PrimeraVistaComponent {
   hola = "holaaa";
+  media = "25 coches al dia"
 }
--- src/assets/js/primeraVista.js
+++ src/assets/js/primeraVista.js
@@ -1,16 +1,58 @@
+let vectorPrueba = [
+    'a'   ,'a'      ,'a'   ,'a'     ,'a'
+    ,'a'  ,true     ,'a'   ,false   ,'a'
+    ,'a'  ,false    ,'a'   ,true    ,'a'
+    ,'a'  ,true     ,'a'   ,true    ,'a'
+    ,'a'  ,false    ,'a'   ,false   ,'a'
+
+]
+
+
 
 function generarTabla() {
     // Obtén una referencia al elemento div
     const miDiv = document.getElementById('contenedorGrande');
-
-    for (let index = 0; index < 11; index++) {
+    let contador = 0;
+    for (let index = 0; index < 25; index++) {
         // Crea un nuevo elemento (por ejemplo, un párrafo)
         const nuevoDiv = document.createElement('div');
-        nuevoDiv.setAttribute('class','content');
-        nuevoDiv.textContent = '¡Hola, mundo!';
+
+        if (vectorPrueba[index] == false) {
+            nuevoDiv.setAttribute('class','content contentVerde');
+            nuevoDiv.textContent = 'P - ' + contador++;
+
+        }else if (vectorPrueba[index] == 'a') {
+            nuevoDiv.setAttribute('class','content contentGris');
+
+        }else{
+            nuevoDiv.setAttribute('class','content');
+            nuevoDiv.textContent = 'P - ' + contador++;
+        }
 
         // Agrega el nuevo párrafo como hijo del div
         miDiv.appendChild(nuevoDiv);
     }
 }
-generarTabla();
+
+function borrarTabla() {
+    const miDiv = document.getElementById('contenedorGrande');
+    while (miDiv.firstChild) {
+        parentElement.removeChild(parentElement.firstChild);
+    }
+}
+
+setInterval(() => {
+    for (let index = 0; index < vectorPrueba.length; index++) {
+        if (vectorPrueba[index] != 'a') {
+           var a = Math.floor(Math.random() * 2)
+           if (a == 1) {
+           console.log(vectorPrueba[index]);
+           vectorPrueba[index] = true;
+           }else{
+                vectorPrueba[index] = false;
+           }
+        }
+    }
+    borrarTabla();
+    generarTabla();
+}, 1000);
